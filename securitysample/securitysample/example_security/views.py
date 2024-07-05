41668ecb6048ea67ba2f8454284fbc8b1b6f6ad6
Kinoshita Hazuki <hkinoshita@knowledge-ex.jp>
2024-07-05 17:14:29 +0900

    7/5 作成分

---
+++ securitysample/securitysample/example_security/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
+@login_required
+def welcome(request):
+    return render(request, 'example_security/welcome.html', {'user': request.user})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('welcome')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'example_security/login.html')
